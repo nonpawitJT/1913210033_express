715f6c5a06f4f05a9d89af45e715f986e486ddca
Nonpawit Nittaworn <1913210033@TNISTUDENT.AC.TH>
2022-11-26 11:21:52 +0700

    Chapter 3 - Workshop A

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.status(200).json({ Fullname: 'Nonpawit Nittaworn' })
+});
+
+router.get('/bio', function (req, res, next) {
+  res.status(200).json({
+    fullname: 'Nonpawit Nittaworn',
+    nickname: 'Tan',
+    hobby: 'Sleep',
+    gitusername: 'nonpawitJT'
+  })
 });
 
 module.exports = router;
