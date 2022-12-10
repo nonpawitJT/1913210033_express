aa16ea23013f7bef9506a57509ba6cde49728534
nonpawitJT <ni.nonpawit_st@tni.ac.th>
2022-12-10 10:59:59 +0700

    Chapter 4 - Connect DB

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company');
 var app = express();
+
+mongoose.connect('mongodb+srv://superdev:9UfyUwkZofsEB9JU@1913210033-tan.pud4w2f.mongodb.net/restfulapi?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(logger('dev'));
 app.use(express.json());
--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,13 +1,9 @@
-exports.company = (req, res, next) =>
+const Company = require('../models/company')
+
+exports.company = async (req, res, next) =>{
+
+    const company = await Company.findOne()
     res.status(200).json({
-        data: [{
-            id: 1, name: "Beartai", address: { province: "Bangkok", postcode: 10320 }
-        },
-        {
-            id: 2, name: "Bitkub", address: { province: "Bangkok", postcode: 10110 }
-        },
-        {
-            id: 3, name: "Station Online", address: { province: "Bangkok", postcode: 10560 }
-        },
-        ]
+        data: company
     })
+}
