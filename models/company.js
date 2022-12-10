f76fa1b5751db0694e9f15fbf4ddd4e50f0fb47c
nonpawitJT <ni.nonpawit_st@tni.ac.th>
2022-12-10 11:35:37 +0700

    Workshop A

--- models/company.js
+++ models/company.js
@@ -6,8 +6,8 @@ const companySchema = new Schema({
     address: {
       province: String,
     }
-  });
+  },{collection:"setting"});
 
-  const company =mongoose.model("Companys",companySchema)
+  const company =mongoose.model("Company",companySchema)
 
   module.exports = company
