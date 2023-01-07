8ea10e22651bd34542503c21839f11c71f657b48
nonpawitJT <ni.nonpawit_st@tni.ac.th>
2023-01-07 11:35:37 +0700

    Chapter 6 - WorkShop B

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,24 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid');
+const { promisify } = require('util')
+const writeFileAsync = promisify(fs.writeFile)
+const config = require('../config/index')
 const Staff = require('../models/staff')
 
 exports.staff = async (req, res, next) => {
 
     const staff = await Staff.find().sort({ _id: -1 })
+
+
+    const staffWithPhotoDomain = await staff.map((staff, index) => {
+        return {
+        name: staff.name,
+        photo: config.DOMAIN+"/images/" + staff.photo,
+         
+        }
+      });
+
+
+
     res.status(200).json({
-        data: staff
+        data: staffWithPhotoDomain
     })
 }
-
 exports.insert = async (req, res, next) => {
 
-    const { name, salary } = req.body
+    const { name, salary, photo } = req.body
+
+    var photoData
+    if (photo == null) {
+        photoData = "nopic.png"
+    } else {
+        photoData = await saveImageToDisk(photo)
+    }
+
     let staff = new Staff({
         name: name,
-        salary: salary
-    });
+        salary: salary,
+        photo: photoData
+
+    })
     await staff.save()
 
     res.status(200).json({
-        message: 'เพิ่มข้อมูลเรียบร้อยแล้ว'
+        message: "เพิ่มข้อมูลเรียบร้อยแล้ว"
     })
 }
 
@@ -100,3 +127,42 @@ exports.update = async (req, res, next) => {
         })
     }
 }
+
+async function saveImageToDisk(baseImage) {
+    //หา path จริงของโปรเจค
+    const projectPath = path.resolve('./') ;
+    //โฟลเดอร์และ path ของการอัปโหลด
+    const uploadPath = `${projectPath}/public/images/`;
+  
+    //หานามสกุลไฟล์
+    const ext = baseImage.substring(baseImage.indexOf("/")+1, baseImage.indexOf(";base64"));
+  
+    //สุ่มชื่อไฟล์ใหม่ พร้อมนามสกุล
+    let filename = '';
+    if (ext === 'svg+xml') {
+        filename = `${uuidv4.v4()}.svg`;
+    } else {
+        filename = `${uuidv4.v4()}.${ext}`;
+    }
+  
+    //Extract base64 data ออกมา
+    let image = decodeBase64Image(baseImage);
+  
+    //เขียนไฟล์ไปไว้ที่ path
+    await writeFileAsync(uploadPath+filename, image.data, 'base64');
+    //return ชื่อไฟล์ใหม่ออกไป
+    return filename;
+  }
+  
+  function decodeBase64Image(base64Str) {
+    var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    var image = {};
+    if (!matches || matches.length !== 3) {
+        throw new Error('Invalid base64 string');
+    }
+  
+    image.type = matches[1];
+    image.data = matches[2];
+  
+    return image;
+  }
--- models/staff.js
+++ models/staff.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const staffSchema = new Schema({
     name: { type: String, require: true, trim: true },
     salary: { type: Number },
-    created: {type : Date, default: Date.now}
+    created: {type : Date, default: Date.now},
+    photo: { type: String, default: 'nopic.png' }
 }, { collection: "staffs" });
 
 const staff = mongoose.model("Staff", staffSchema)
